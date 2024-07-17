1aa55699e6a63682f8760e80b541ba29391c5ded
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-07-17 14:45:56 +0200

    final?

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,4 +1,5 @@
 const params = new URL(window.location).searchParams;
+if(params.get("orderId") == null || params.get("orderId") == undefined)window.location="./index.html";
 let order = params.get("orderId");
 
 orderId.innerHTML = order;
